d31d5f458015dd2c68888127b78062e017d252cb
sitahald <sitahald@sheridancollege.ca>
2025-10-20 23:00:32 -0400

    1 change

---
+++ src/main/java/git/midterm2/Midterm2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package git.midterm2;
+
+/**
+ *
+ * @author wwjde
+ */
+public class Midterm2 {
+
+    public static void main(String[] args) {
+        System.out.println("first");
+    }
+}
